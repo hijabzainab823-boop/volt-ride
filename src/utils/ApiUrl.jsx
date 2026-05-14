94615c90d5fa0410368b0181a73e95e0a43a8545
Hijab Zainab <hijabzainab823@gmail.com>
2026-05-14 13:05:34 +0500

    "URL"

--- src/utils/ApiUrl.jsx
+++ src/utils/ApiUrl.jsx
@@ -1,3 +1,3 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://volt-server-rosy.vercel.app/api";
-export const FILE_URL = import.meta.env.VITE_FILE_URL || "https://volt-server-rosy.vercel.app/";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://volt-server-drab.vercel.app/api";
+export const FILE_URL = import.meta.env.VITE_FILE_URL || "https://volt-server-drab.vercel.app/";
 export const STRIPE_PUBLIC_KEY = "pk_test_51SV9DUCqqxjsEQuelFHQpnQTfkdrN3e2Q1qFpDeM9JTVNeke3X9Iq47MhzJHLAU6gSs4v8xQfvISg1R1ojtMnwVr002j4FDvWu";
